d8c453f51298c773d8636cd52cb9ddc100716284
Daniel Crenna <daniel.crenna@gmail.com>
2020-11-23 18:05:18 -0500

    nit: shaping

--- DanielCrenna/Client/Program.cs
+++ DanielCrenna/Client/Program.cs
@@ -1,9 +1,3 @@
-// Copyright (c) Daniel Crenna. All rights reserved.
-// 
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, you can obtain one at http://mozilla.org/MPL/2.0/.
-
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -17,10 +11,9 @@ namespace DanielCrenna.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(
-                sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
