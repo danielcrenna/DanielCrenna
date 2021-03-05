f8fa708149b2cb93224325acefb25c00ef590bc5
Daniel Crenna <daniel.crenna@gmail.com>
2021-03-05 13:18:54 -0500

    add scope detection

--- tests/BetterTesting.Tests/WebApplicationFactoryExtensions.cs
+++ tests/BetterTesting.Tests/WebApplicationFactoryExtensions.cs
@@ -8,8 +8,10 @@ using System;
 using System.Text;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using Xunit.Abstractions;
 
 namespace BetterTesting.Tests
@@ -31,8 +33,13 @@ namespace BetterTesting.Tests
             {
                 builder.ConfigureLogging(logging =>
                 {
-                    logging.ClearProviders(); // remove other logging providers, such as remote loggers or unnecessary event logs
-                    logging.Services.AddSingleton<ILoggerProvider>(r => new XunitLoggerProvider(output));
+                    // check if scopes are used in normal operation
+                    var useScopes = logging.UsesScopes();
+
+                    // remove other logging providers, such as remote loggers or unnecessary event logs
+                    logging.ClearProviders();
+
+                    logging.Services.AddSingleton<ILoggerProvider>(r => new XunitLoggerProvider(output, useScopes));
                 });
             });
         }
@@ -48,16 +55,18 @@ namespace BetterTesting.Tests
             private const string Warn = "warn";
             private const string Error = "fail";
             private const string Critical = "crit";
+            
             private readonly string _categoryName;
-
+            private readonly bool _useScopes;
             private readonly ITestOutputHelper _output;
             private readonly IExternalScopeProvider _scopes;
 
-            public XunitLogger(ITestOutputHelper output, IExternalScopeProvider scopes, string categoryName)
+            public XunitLogger(ITestOutputHelper output, IExternalScopeProvider scopes, string categoryName, bool useScopes)
             {
                 _output = output;
                 _scopes = scopes;
                 _categoryName = categoryName;
+                _useScopes = useScopes;
             }
 
             public bool IsEnabled(LogLevel logLevel)
@@ -103,7 +112,7 @@ namespace BetterTesting.Tests
 
                 sb.Append(": ").Append(_categoryName).Append('[').Append(eventId).Append(']').AppendLine();
 
-                if (TryAppendScopes(sb))
+                if (_useScopes && TryAppendScopes(sb))
                     sb.AppendLine();
 
                 sb.Append(Spacer);
@@ -141,16 +150,19 @@ namespace BetterTesting.Tests
         private sealed class XunitLoggerProvider : ILoggerProvider, ISupportExternalScope
         {
             private readonly ITestOutputHelper _output;
+            private readonly bool _useScopes;
+
             private IExternalScopeProvider _scopes;
 
-            public XunitLoggerProvider(ITestOutputHelper output)
+            public XunitLoggerProvider(ITestOutputHelper output, bool useScopes)
             {
                 _output = output;
+                _useScopes = useScopes;
             }
 
             public ILogger CreateLogger(string categoryName)
             {
-                return new XunitLogger(_output, _scopes, categoryName);
+                return new XunitLogger(_output, _scopes, categoryName, _useScopes);
             }
 
             public void Dispose()
@@ -161,6 +173,33 @@ namespace BetterTesting.Tests
             {
                 _scopes = scopes;
             }
+        }
+
+        private static bool UsesScopes(this ILoggingBuilder builder)
+        {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+
+            // look for other host builders on this chain calling ConfigureLogging explicitly
+            var options = serviceProvider.GetService<SimpleConsoleFormatterOptions>() ??
+                          serviceProvider.GetService<JsonConsoleFormatterOptions>() ??
+                          serviceProvider.GetService<ConsoleFormatterOptions>();
+
+            if (options != default)
+                return options.IncludeScopes;
+
+            // look for other configuration sources
+            // See: https://docs.microsoft.com/en-us/dotnet/core/extensions/logging?tabs=command-line#set-log-level-by-command-line-environment-variables-and-other-configuration
+
+            var config = serviceProvider.GetService<IConfigurationRoot>() ?? serviceProvider.GetService<IConfiguration>();
+            var logging = config?.GetSection("Logging");
+            if (logging == default)
+                return false;
+
+            var includeScopes = logging?.GetValue("Console:IncludeScopes", false);
+            if (!includeScopes.Value)
+                includeScopes = logging?.GetValue("IncludeScopes", false);
+
+            return includeScopes.GetValueOrDefault(false);
         }
 
         #endregion
